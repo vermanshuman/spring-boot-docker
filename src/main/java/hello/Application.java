ab9364c3c0264211bd372c90b20d5177a6a83743
EC2 Default User <ec2-user@ip-172-31-45-81.us-west-2.compute.internal>
2016-12-14 07:02:06 +0000

    testing

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
+     
+
 		SpringApplication.run(Application.class, args);
 	}
 
